78a3775df64c587df9551a73ef4c1475a9cf3cd9
hcldevops <tyagi.h@hcl.com>
2014-06-26 16:04:09 +0530

    signin out finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,12 @@ class SessionsController < ApplicationController
   def new
   end
 
- def create
+ def create 	
  	user = User.find_by(email: params[:session][:email].downcase)
- 	if user && user.authenticate(params[:session][:password])
- 		flash.now[:success] = "Welcome to dashboard!!"
-	 	render 'show'
+    if user && user.authenticate(params[:session][:password])
+ 		sign_in user
+ 		redirect_to user
+ 	
 	else
 		flash.now[:error] = "Invalid credentials!! Login again"
 		render 'new'
@@ -14,5 +15,7 @@ class SessionsController < ApplicationController
 	end
  end
   def destroy
+  	sign_out
+    redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
    @user = User.new(user_params)
    if @user.save
+    
    	flash[:success] = "Announcement created!"
       redirect_to root_url
    else
@@ -14,8 +15,7 @@ class UsersController < ApplicationController
   end
 
   def show
-  	render new
-    end
+  end
 
   private
   def user_params
