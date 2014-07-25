3b2727599dbb0f6f73cdb5fe00bc531813a5fc71
Jarvis <bibin.w@hcl.com>
2014-07-25 14:30:00 +0530

    cookbook delete functionality added to index

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -1,7 +1,7 @@
 class CookbooksController < ApplicationController
   before_action :set_cookbook, only: [:show, :edit, :update, :destroy]
-  Script_dir = '/home/tyagi/scripts'
-  Repo_dir = '/home/tyagi/workspace/cookmaster/public/cookbooks/'
+  Script_dir = '/home/cookmaster/scripts'
+  Repo_dir = '/home/cookmaster/sites/cookmaster/public/cookbooks/'
   # Cookbook_path = 'RAILS.ROOT'
   
 
@@ -39,7 +39,7 @@ def create
 
     @cookbook = Cookbook.new(cookbook_params)
     
-    system "echo Admin098 | sudo sh #{Script_dir}/create_cookbook.sh #{@cookbook.name} #{Repo_dir}"
+    system "echo bibinmtech | sudo sh #{Script_dir}/create_cookbook.sh #{@cookbook.name} #{Repo_dir}"
     @cookbook.path = "#{Repo_dir}/#{@cookbook.name}"
     
     respond_to do |format|
